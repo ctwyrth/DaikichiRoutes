30806d15d3b52bac0b8a9a016b957881a84c0b90
Thomas W <96929397+ctwyrth@users.noreply.github.com>
2022-04-10 00:02:47 -0700

    First commit

---
+++ src/main/java/com/ctwyrth/daikichiroutes/HomeController.java
@@ -0,0 +1,29 @@
+package com.ctwyrth.daikichiroutes;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	
+	@RequestMapping("/")
+	public String hello() {
+		return "Hello World!";
+	}
+	
+	@RequestMapping("/daikichi")
+	public String welcome() {
+		return "Welcome!";
+	}
+	
+	@RequestMapping("/daikichi/today")
+	public String today() {
+		return "Today you will find luck in all your endeavors!";
+	}
+	
+	@RequestMapping("/daikichi/tomorrow")
+	public String tomorrow() {
+		return "Tomorrow an opportunity will arise, so be sure to be open to new ideas!";
+	}
+
+}
